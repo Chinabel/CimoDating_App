ba283a79928c32e30c2969e98c9afbb86b5fcffc
Chineze ogugua <106745705+Chinabel@users.noreply.github.com>
2023-12-02 22:42:24 +0100

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,1 +1,52 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const history = useHistory();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('/users/login', { username, password }); // Update the endpoint based on your API
+
+      // Assuming your backend sends a token upon successful login
+      const token = response.data.token;
+
+      // Store the token in local storage or cookies for subsequent requests
+      localStorage.setItem('token', token);
+
+      // Redirect to the user's profile or another page
+      history.push('/profile');
+    } catch (error) {
+      setError('Invalid username or password');
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+      <form onSubmit={handleLogin}>
+        <label>
+          Username:
+          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} required />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+        </label>
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
 
