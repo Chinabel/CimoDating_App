d3a55b1472367a8aa326ed6772f7570f4c1ecdd2
Chineze ogugua <106745705+Chinabel@users.noreply.github.com>
2023-12-02 22:33:34 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost:27017/dating_app', { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
