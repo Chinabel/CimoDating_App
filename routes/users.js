c0b2f31289108bfa6cd10e9b20e1027692343d65
Chineze ogugua <106745705+Chinabel@users.noreply.github.com>
2023-12-02 22:35:47 +0100

    Create users.js

---
+++ routes/users.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+
+// Define routes for user registration, login, etc.
+
+module.exports = router;
