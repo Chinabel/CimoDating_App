ca5c74a10f851988d5a9197477f58ff5ae81cda9
Chineze ogugua <106745705+Chinabel@users.noreply.github.com>
2023-12-02 22:41:20 +0100

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,1 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Home = () => {
+  const [matches, setMatches] = useState([]);
+
+  useEffect(() => {
+    // Fetch potential matches from the backend
+    axios.get('/users/matches')  // Update the endpoint based on your API
+      .then(response => {
+        setMatches(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching matches:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h2>Potential Matches</h2>
+      <ul>
+        {matches.map(match => (
+          <li key={match._id}>
+            <div>
+              <h3>{match.username}</h3>
+              {/* Display other match details (e.g., age, interests, etc.) */}
+            </div>
+            {/* Add buttons or actions for the user to interact with the match */}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Home;
 
